754bb20473767cf62b68af2580893420701677a2
mikhaillukovtsev <mikhail.lukovtse2007@gmail.com>
2025-10-21 15:39:38 +0300

    Исправлены ошибки

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,26 +1,7 @@
-#!/usr/bin/env python3
-"""
-Модуль для демонстрации работы с текстовыми сообщениями.
-"""
+"Пояснение работы"
 
 
-def get_message():
-    """
-    Возвращает стандартное приветственное сообщение.
-
-    Returns:
-        str: Строка с текстом приветствия
-    """
-    return "Hello World!"
-
-
-def main():
-    """
-    Основная функция для запуска модуля.
-    """
-    message = get_message()
-    print(message)
-
-
-if __name__ == "__main__":
-    main()
+def text():
+    """Функция выводит сообщение message"""
+    message = "message"
+    return message
