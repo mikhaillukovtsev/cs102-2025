ba8afec749baa5606c06617f49217700021f0031
mikhaillukovtsev <mikhail.lukovtse2007@gmail.com>
2025-11-13 20:18:32 +0300

    Реализованы функция encrypt_vigenere() и encrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,27 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    if len(keyword) < len(plaintext):
+        keyword *= len(plaintext) // len(keyword) + 1
+    i = 0
+    for l in plaintext: 
+        if keyword[i].isupper():
+            k = ord(keyword[i]) - 65
+        else:
+            k = ord(keyword[i]) - 97
+        if 65 <= ord(l) <= 90:
+            if ord(l) + k > 90:
+                ciphertext += chr((ord(l) + k - 90) + 64)
+            else:
+                ciphertext += chr(ord(l) + k)
+        elif 97 <= ord(l) <= 122:
+            if ord(l) + k > 122:
+                ciphertext += chr((ord(l) + k - 122) + 96)
+            else:
+                ciphertext += chr(ord(l) + k)
+        else:
+            ciphertext += l
+        i += 1
     return ciphertext
 
 
@@ -24,5 +44,25 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    if len(keyword) < len(ciphertext):
+        keyword *= len(ciphertext) // len(keyword) + 1
+    i = 0
+    for l in ciphertext: 
+        if keyword[i].isupper():
+            k = ord(keyword[i]) - 65
+        else:
+            k = ord(keyword[i]) - 97
+        if 65 <= ord(l) <= 90:
+            if ord(l) - k < 65:
+                plaintext += chr(91 - (65 - (ord(l) - k)))
+            else:
+                plaintext += chr(ord(l) - k) 
+        elif 97 <= ord(l) <= 122:
+            if ord(l) - k < 97:
+                plaintext += chr(123 - (97 - (ord(l) - k)))
+            else:
+                plaintext += chr(ord(l) - k)
+        else:
+            plaintext += l
+        i += 1
     return plaintext
