8b8142e325b5f910e4c6bc11c4b120f8aa50a986
mikhaillukovtsev <mikhail.lukovtse2007@gmail.com>
2026-01-11 17:47:10 +0300

    исправлены комментарии

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -40,10 +40,7 @@ def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
-    groups = []
-    for i in range(0, len(values), n):
-        groups.append(values[i : i + n])
-    return groups
+    return [values[i : i + n] for i in range(0, len(values), n)]
 
 
 def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
@@ -55,7 +52,7 @@ def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str
     >>> get_row([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (2, 0))
     ['.', '8', '9']
     """
-    row, col = pos
+    row, _ = pos
     return grid[row]
 
 
@@ -68,11 +65,8 @@ def get_col(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str
     >>> get_col([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']], (0, 2))
     ['3', '6', '9']
     """
-    row, col = pos
-    result = []
-    for i in grid:
-        result.append(i[col])
-    return result
+    _, col = pos
+    return [row[col] for row in grid]
 
 
 def get_block(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
@@ -106,9 +100,9 @@ def find_empty_positions(
     >>> find_empty_positions([['1', '2', '3'], ['4', '5', '6'], ['.', '8', '9']])
     (2, 0)
     """
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == ".":
+    for i, row in enumerate(grid):
+        for j, cell in enumerate(row):
+            if cell == ".":
                 return (i, j)
     return None
 
@@ -124,18 +118,8 @@ def find_possible_values(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -
     True
     """
     base = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
-    imp_values = set()
-    for i in get_row(grid, pos):
-        if i != ".":
-            imp_values.add(i)
-    for i in get_col(grid, pos):
-        if i != ".":
-            imp_values.add(i)
-    for i in get_block(grid, pos):
-        if i != ".":
-            imp_values.add(i)
-    values = base - imp_values
-    return values
+    imp_values = (set(get_row(grid, pos)) | set(get_col(grid, pos)) | set(get_block(grid, pos))) - {"."}
+    return base - imp_values
 
 
 def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
@@ -151,16 +135,17 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
     pos = find_empty_positions(grid)
-    if pos is None:
-        return grid
-    values = find_possible_values(grid, pos)
+    if not pos:
+        return [row[:] for row in grid]
+    possible_values = find_possible_values(grid, pos)
     row, col = pos
-    for i in values:
-        grid_test = [r[:] for r in grid]
-        grid_test[row][col] = i
-        sol = solve(grid_test)
-        if sol is not None:
-            return sol
+    for value in possible_values:
+        original_value = grid[row][col]
+        grid[row][col] = value
+        solution = solve(grid)
+        grid[row][col] = original_value
+        if solution:
+            return solution
     return None
 
 
@@ -168,17 +153,15 @@ def check_solution(solution: tp.List[tp.List[str]]) -> bool:
     """Если решение solution верно, то вернуть True, в противном случае False"""
     # TODO: Add doctests with bad puzzles
     base = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
-    for row in solution:
-        if set(row) != base:
+    for r in range(9):
+        if set(get_row(solution, (r, 0))) != base:
             return False
-    for i in range(9):
-        col = [row[i] for row in solution]
-        if set(col) != base:
+    for c in range(9):
+        if set(get_col(solution, (0, c))) != base:
             return False
-    for i in range(0, 9, 3):
-        for j in range(0, 9, 3):
-            block = [solution[r][c] for r in range(i, i + 3) for c in range(j, j + 3)]
-            if set(block) != base:
+    for br in range(0, 9, 3):
+        for bc in range(0, 9, 3):
+            if set(get_block(solution, (br, bc))) != base:
                 return False
     return True
 
